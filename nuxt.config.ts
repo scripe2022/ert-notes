117444f1c14098e96fc6ff15b6ea1cd51406c6e2
scripe2022 <im@jyh.sb>
2022-01-23 00:56:36 -0800

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     // ssr: false,
-    // modules: [
-    //     // Using package name
-    //     '@vueup/vue-quill'
-    // ],
-    // meta: {
-    //     script: [
-    //         { src: 'https://cdn.quilljs.com/1.3.6/quill.js' }
-    //     ],
-    //     link: [
-    //         { rel: 'stylesheet', href: 'https://cdn.quilljs.com/1.3.6/quill.snow.css' }
-    //     ],
-    // },
     serverMiddleware: {
         '/server': '~/server/middleware'
     },
